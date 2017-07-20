71fd7956300d4de7a33d88b858558c179f93d3e1
DivyaVuppala <divya.vuppala0603@gmail.com>
2017-07-20 00:49:27 -0700

    new file added

---
+++ oscommands.py
@@ -0,0 +1,8 @@
+# import os
+# dir=os.popen("ps -axo pid,ppid,%mem,%cpu | head -n 10 ").readlines()
+# for i in dir:
+# 	print(i)
+
+
+import subprocess
+subprocess.call("/Users/divyavuppala/documents/awksedpractice.sh")
